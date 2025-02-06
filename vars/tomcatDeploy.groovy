dc7e51a8c0c688f93c640d867f843690513333ed
ruchita3107 <rruucchhii31@gmail.com>
2025-02-06 14:48:08 -0600

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,7 +1,6 @@
 def call(ip,warName,credId){
-  sshagent(["${credId}}) {
-   sh "scp target/${warName}.war ec2-user@${ip}:/opt/tomcat10/webapps" 
-   sh "ssh ec2-user@${ip} /opt/tomcat10/bin/shutdown.sh"
-   sh "ssh ec2-user@${ip} /opt/tomcat10/bin/startup.sh" 
+  sshPublisher(publishers: [sshPublisherDesc(configName: 'tomcat10', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '''/opt/tomcat10/bin/shutdown.sh
+/opt/tomcat10/bin/startup.sh''', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '/opt/tomcat10/webapps', remoteDirectorySDF: false, removePrefix: '/target', sourceFiles: '/target/*.war')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
+  
        }
             }
