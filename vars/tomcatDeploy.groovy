0e0b1c962074456439f39d6da58063a653d64f2c
ruchita3107 <rruucchhii31@gmail.com>
2025-02-06 15:07:52 -0600

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,4 +1,4 @@
-def call(ip,warName,credId){
+def call(ip,warName){
   sshPublisher(publishers: [sshPublisherDesc(configName: 'tomcat10', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '''/opt/tomcat10/bin/shutdown.sh
 /opt/tomcat10/bin/startup.sh''', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '/opt/tomcat10/webapps', remoteDirectorySDF: false, removePrefix: '/target', sourceFiles: '/target/*.war')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
   
