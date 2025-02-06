65b93bdba4fb1217caa4adf25df510512424e58a
ruchita3107 <rruucchhii31@gmail.com>
2025-02-06 15:19:15 -0600

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,6 +1,30 @@
-def call(ip,warName){
-  sshPublisher(publishers: [sshPublisherDesc(configName: 'tomcat10', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '''/opt/tomcat10/bin/shutdown.sh
-/opt/tomcat10/bin/startup.sh''', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '/opt/tomcat10/webapps', remoteDirectorySDF: false, removePrefix: '/target', sourceFiles: '/target/*.war')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
-  
-       }
-            
+def call(ip, warName) {
+    sshPublisher(
+        publishers: [
+            sshPublisherDesc(
+                configName: 'tomcat10', 
+                transfers: [
+                    sshTransfer(
+                        cleanRemote: false,  // Don't clean the remote directory before transferring files
+                        excludes: '',        // No files excluded
+                        execCommand: '''/opt/tomcat10/bin/shutdown.sh
+                                        /opt/tomcat10/bin/startup.sh''',  // Shutdown and start Tomcat
+                        execTimeout: 120000,  // Timeout for executing commands (2 minutes)
+                        flatten: false,       // Keep the directory structure
+                        makeEmptyDirs: false, // Don't create empty directories
+                        noDefaultExcludes: false, // Use default excludes
+                        patternSeparator: '[, ]+', // Pattern separator for file patterns
+                        remoteDirectory: '/opt/tomcat10/webapps', // Remote directory where the war file goes
+                        remoteDirectorySDF: false, // Don't use a timestamp directory for remote directory
+                        removePrefix: '/target',  // Remove '/target' prefix before uploading files
+                        sourceFiles: '/target/*.war'  // The source files to upload
+                    )
+                ],
+                usePromotionTimestamp: false, // Don't use promotion timestamp
+                useWorkspaceInPromotion: false, // Don't use the workspace in promotion
+                verbose: false // Don't enable verbose logging
+            )
+        ]
+    )
+}
+
